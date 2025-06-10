b78dac2c272a51498af2e32f590fc49864cbe88c
Francisco Albarran <francisco.albarran@solera.com>
2025-06-09 19:06:37 -0600

    Section 5: Client login and register

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../services/account.service';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  private accountService = inject(AccountService); 
+  cancelRegister = output<boolean>();
+ 
+  model: any = {};
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log('Registration successful:', response);
+        this.cancel();
+      },
+      error: error => {
+        console.error('Registration failed:', error);
+        alert('Registration failed. Please try again.');
+      }
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('Registration cancelled');
+  }
+
+}
